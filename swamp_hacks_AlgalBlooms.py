625ccef4c586edbaff183187efb36f1be6bedca0
julianstennett <116732033+julianstennett@users.noreply.github.com>
2023-01-28 15:13:39 -0500

    Add files via upload
    updated gui to a modern look
    working on map functionality and concept using tkintermapview framework

--- swamp_hacks_AlgalBlooms.py
+++ swamp_hacks_AlgalBlooms.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+import swamp_hacks_Cl.cpp
 url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=c6f760be-b94f-495e-aa91-2d8e6f426e11&limit=5'  
 fileobj = urllib.request.urlopen(url)
 response_dict = json.loads(fileobj.read())
@@ -14,8 +15,10 @@ class AlgalBloom:
         self.lat = latitude
         self.lon = longitude
 
-for i in range(len(data)):
-    print(f"OfficialWaterBodyName: {data[i]['OfficialWaterBodyName']}")
-    print(f"Latitude: {data[i]['Longitude']}")
-    print(f"Longitude: {data[i]['Latitude']}")
-    print()
+
+def DataClass():
+    for i in range(len(data)):
+        print(f"OfficialWaterBodyName: {data[i]['OfficialWaterBodyName']}")
+        print(f"Latitude: {data[i]['Longitude']}")
+        print(f"Longitude: {data[i]['Latitude']}")
+        print()
--- swamp_hacks_GUI.py
+++ swamp_hacks_GUI.py
@@ -1,12 +1,23 @@
-import tkinter as tk
+import customtkinter
 
 class AlgalBloomGui:
+    customtkinter.set_appearance_mode("dark")
+    customtkinter.set_default_color_theme("dark-blue")
 
     def __init__(self):
-        self.root = tk.Tk()
+        self.root = customtkinter.CTk()
+
+        frame = customtkinter.CTkFrame(master=self.root)
+        frame.pack(pady=20, padx=20, fill="both", expand=True)
         
-        self.root.geometry("300x300")
+        self.root.geometry("900x900")
+
+        self.button = customtkinter.CTkButton(self.root, text=".", font=('Ariel', 1), command=self.click)
+        self.button.pack(padx=1, pady=1)
 
         self.root.mainloop()
+    
+    def click(self):
+        print("you clicked lil bro!")
 
 AlgalBloomGui()
