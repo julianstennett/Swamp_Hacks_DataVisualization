86e9a189a71e5c29fe6b56f8638e248cfe12649c
julianstennett <116732033+julianstennett@users.noreply.github.com>
2023-01-28 12:48:18 -0500

    Add files via upload
    - started with importing data from the public dataset into AlgalBlooms file
    - started basic GUI for plotting data

---
+++ swamp_hacks_AlgalBlooms.py
@@ -0,0 +1,15 @@
+import json
+import urllib.request
+url = 'https://data.ca.gov/api/3/action/datastore_search?resource_id=c6f760be-b94f-495e-aa91-2d8e6f426e11&limit=5'  
+fileobj = urllib.request.urlopen(url)
+response_dict = json.loads(fileobj.read())
+
+data = response_dict["result"] # create new object data which holds the results from the response dict
+data = data["records"]
+print()
+
+for i in range(len(data)):
+    print(f"OfficialWaterBodyName: {data[i]['OfficialWaterBodyName']}")
+    print(f"Latitude: {data[i]['Longitude']}")
+    print(f"Longitude: {data[i]['Latitude']}")
+    print()
---
+++ swamp_hacks_GUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("1000x1000")
+
+label = tk.Label(root, text="Algal Blooms", font=('Arial', 18))
+label.pack()
+
+
+root.mainloop()
